8cf7616854f596f604de5af60a2063e2f3d000c6
iamrosada0 <luisrosada@outlook.com>
2025-05-16 13:21:33 +0100

    chore: Implementing the aws_sqs_queue with terraform_queue

--- terraform/localstack/main.tf
+++ terraform/localstack/main.tf
@@ -26,6 +26,11 @@ variable "bucket_name" {
   type    = string
   default = "my-test-bucket"
 }
+
+variable "sqs_queue_name" {
+  type    = string
+  default = "my-custom-sqs-queue"
+}
 provider "aws" {
   access_key                  = var.access_key
   secret_key                  = var.secret_key
@@ -43,4 +48,13 @@ provider "aws" {
 
 resource "aws_s3_bucket" "test-bucket" {
   bucket = var.bucket_name
+}
+
+resource "aws_sqs_queue" "terraform_queue" {
+  name                      = var.sqs_queue_name
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+ 
 }
